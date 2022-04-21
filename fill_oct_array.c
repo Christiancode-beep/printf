78529336fcbc515ebaf120fd2494bf91f7018c82
Christiancode-beep <christianobinna71@gmail.com>
2022-04-21 00:29:39 -0700

    A function that writes characters

--- fill_oct_array.c
+++ fill_oct_array.c
@@ -13,6 +13,7 @@ char *fill_oct_array(char *bnr, char *oct)
 
 	oct[11] = '\0';
 	for (i = 31, ioct = 10; i >= 0; i--, ioct--)
+	
 	{
 		if (i > 1)
 			limit = 4;
@@ -21,7 +22,8 @@ char *fill_oct_array(char *bnr, char *oct)
 		for (op = 0, j = 1; j <= limit; j *= 2, i--)
 			op = ((bnr[i] - '0') * j) + op;
 		i++;
-		oct[ioct] = op + '0';
+	oct[ioct] = op + '0';
 	}
+
 	return (oct);
 }
